7d94c85335e50ae4acdb7bc5a8ca4e7749422512
Vincent Welbourne <vincent.vw04@gmail.com>
2025-10-19 17:55:35 -0400

    Add constructor, getters, toString and JavaDocs
    Created the constructor that declares a card contains a Value (The worth
    o a card), Suit (If the card is Clubs, Diamonds..) and lastly Rank (Ace,
    King, Jack..). Added Getters for access. Added toString so that each
    card has its name for example Ace of Hearts. Finally added JavaDocs to
    everything for easy reading and convention.

--- src/Card.java
+++ src/Card.java
@@ -1,5 +1,69 @@
+/*
+ * Program created by Vincent Welbourne
+ * vincent.vw04@gmail.com
+ *
+ */
 public abstract class Card {
-    private int value;		// Worth of the card
-    private String suit;	// Clubs, Diamonds etc..
-    private String rank; 	// Ace, 2, king etc..
+    /**
+     * The value of the card in the deck
+     * ex: 10 of spades = 10, king of hearts = 10.
+     */
+    private final int value;		// Worth of the card
+
+    /**
+     * The suit of the card in the deck
+     * ex: clubs, diamonds...
+     */
+    private final String suit;	// Clubs, Diamonds etc..
+
+    /**
+     * The rank of the card in the deck
+     * ex: ace, 2, king...
+     */
+    private final String rank; 	// Ace, 2, king etc..
+
+    /**
+     * Card constructor. Sets the fields of the requirements of what makes a card.
+     * @param value Value of the actual card..
+     * @param suit The suit of the card ex: Clubs, Diamonds..
+     * @param rank The rank of the card ex: Ace, 2, King..
+     */
+    protected Card(int value, String suit, String rank) {
+        this.value = value;
+        this.suit = suit;
+        this.rank = rank;
+    }
+
+    /**
+     * Getter for Value
+     * @return value
+     */
+    public int getValue() {
+        return value;
+    }
+
+    /**
+     * Getter for Suit
+     * @return Suit
+     */
+    public String getSuit() {
+        return suit;
+    }
+
+    /**
+     * Getter for Rank
+     * @return Rank
+     */
+    public String getRank() {
+        return rank;
+    }
+
+    /**
+     * To get the string name of the card (Ace of Hearts)
+     * @return String
+     */
+    @Override
+    public String toString() {
+        return rank + " of " + suit;
+    }
 }
